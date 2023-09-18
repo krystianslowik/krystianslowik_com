5282d09a426c5198e6ffe4f9e33779bf6419fb64
krystianslovik <krystian.slowik@trustedshops.com>
2023-09-18 21:43:16 +0200

    versioning and (c)

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,6 +1,10 @@
 import React from "react";
+import packageJson from "../../package.json";
 
 const InfoBox = () => {
+  const currentYear = new Date().getFullYear();
+  const { version } = packageJson;
+
   return (
     <div className="flex justify-center items-center text-gray-400/50 text-xs my-4 w-full">
       <span>
@@ -14,7 +18,10 @@ const InfoBox = () => {
           Privacy Policy
         </a>
         . <br />
-        <b>Conversation is stored </b>for training purposes.
+        <b>Conversation is stored </b>for training purposes. <br />
+        <div className="mt-3 text-gray-400/30">
+          &copy;{` ${currentYear} v${version}`}
+        </div>
       </span>
     </div>
   );
