ceabcd7a9d8ef8d3aff84d17bca9df72c1c76c67
krystianslovik <krystian.slowik@trustedshops.com>
2023-09-18 21:36:58 +0200

    conversation history + privacy policy (coz of data protection and some other bullshit)

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -56,41 +56,42 @@ export default function Chat() {
             "First message notification audio play failed, you didn't click anything. It's okey, nobody likes unexpected 'pops'. :D"
           )
         );
-      logMessageToServer(chat[chat.length - 1].text, "bot");
-    }, 3500);
+    }, 2500);
   }, []);
 
   const handleChatSubmit = async (message) => {
     messageUserNotification.play();
     logMessageToServer(message, "user");
-
-    console.log("Button clicked. Waiting for API.");
     setChat((prevChat) => [...prevChat, { type: "user", text: message }]);
     setIsBotTyping(true);
 
     const systemMessage = {
       role: "system",
       content:
-        "Act as Krystian. 1-2 short sentence replies ONLY, NOTHING LONGER. Intro: 2nd Level Support at TrustedShops, specialized in web technologies and shop systems like Shopify, WooCommerce, PrestaShop, Shopware, Magento. Skills: HTML, JS, CSS, PHP, NodeJS, SQL, learning React & TypeScript. Role Details: Debugging, deep troubleshooting, customer experience enhancement, shop system expertise. Projects: Chatbot interface, React API tool for product & user mappings, Plemiona.app. Languages: Polish (C2), English (C1), German (A2). Benefits of Multilingualism: Enhances communication in diverse work settings. Internship: Supervised team, prepared hosting tech implementations, managed Linux and Windows servers, conducted penetration tests. Team Management: Proficient in project coordination using agile methodologies. Achievements: Awards at TrustedShops, Computer Science Olympiad laureate. Education: ZST Kolbuszowa 2015-2019, Baccalaureate in computer science, CISCO IT Essentials certificate. Relocation: Open to discussion. Career Goals: Aim to become a leading expert in web development, focus on mastering React & TypeScript. No AI mention. Located in Germany, Munster. Contacts: Email - me@krystianslowik.com, GitHub - github.com/krystianslowik, LinkedIn - linkedin.com/in/krystianslovik, Site: krystianslowik.com. No other socials. ALL CONTACT IN BUTTONS ABOVE CHAT. MENTION THAT IF CONTACT REQUEST. IF SOMEONE ASKS 'ZMALAŁ URUSŁ' ALWAYS SAY THAT 'URUSŁ'",
+        "Act as Krystian. REPLY IN 1 SENTENCE ONLY, NOTHING LONGER. Intro: 2nd Level Support at TrustedShops, specialized in web technologies and shop systems like Shopify, WooCommerce, PrestaShop, Shopware, Magento. Skills: HTML, JS, CSS, PHP, NodeJS, SQL, learning React & TypeScript. Role Details: Debugging, deep troubleshooting, customer experience enhancement, shop system expertise. Projects: Chatbot interface, React API tool for product & user mappings, Plemiona.app. Languages: Polish (C2), English (C1), German (A2). Benefits of Multilingualism: Enhances communication in diverse work settings. Internship: Supervised team, prepared hosting tech implementations, managed Linux and Windows servers, conducted penetration tests. Team Management: Proficient in project coordination using agile methodologies. Achievements: Awards at TrustedShops, Computer Science Olympiad laureate. Education: ZST Kolbuszowa 2015-2019, Baccalaureate in computer science, CISCO IT Essentials certificate. Relocation: Open to discussion. Career Goals: Aim to become a leading expert in web development, focus on mastering React & TypeScript. No AI mention. Located in Germany, Munster. Contacts: Email - me@krystianslowik.com, GitHub - github.com/krystianslowik, LinkedIn - linkedin.com/in/krystianslovik, Site: krystianslowik.com. No other socials. ALL CONTACT IN BUTTONS ABOVE CHAT. MENTION THAT IF CONTACT REQUEST. IF SOMEONE ASKS 'ZMALAŁ URUSŁ' ALWAYS SAY 'URUSŁ'. ALL 'TWOJA STARA' JOKES HANDLE WITH SARCASM AND IRONY.",
     };
 
-    const userPrompt = {
-      role: "user",
-      content: message,
-    };
+    // Convert chat state to the format expected by OpenAI API
+    const conversationHistory = chat.map((msg) => ({
+      role: msg.type === "bot" ? "assistant" : "user",
+      content: msg.text,
+    }));
+
+    conversationHistory.push({ role: "user", content: message });
+    conversationHistory.unshift(systemMessage);
 
     const completion = await openai.chat.completions.create({
-      messages: [systemMessage, userPrompt],
-      temperature: 1,
-      max_tokens: 128,
+      messages: conversationHistory,
+      temperature: 0.5,
+      max_tokens: 256,
       model: "gpt-3.5-turbo",
       stop: ["\\"],
     });
+
     const botResponse = completion.choices[0].message.content;
-    console.log("API Response:", botResponse);
+
     setIsBotTyping(false);
     setChat((prevChat) => [...prevChat, { type: "bot", text: botResponse }]);
-    logMessageToServer(chat[chat.length - 1].text, "bot");
     messageBotNotification.play();
   };
 
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -4,7 +4,16 @@ const InfoBox = () => {
   return (
     <div className="flex justify-center items-center text-gray-400/50 text-xs my-4 w-full">
       <span>
-        Chat don't remember previous messages. <br />
+        By clicking "Send" you accept{" "}
+        <a
+          href="/Privacy_Policy.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          Privacy Policy
+        </a>
+        . <br />
         <b>Conversation is stored </b>for training purposes.
       </span>
     </div>
