021acc6b9114b316b23b58ec4b09c813bae97125
krystianslovik <krystian.slowik@trustedshops.com>
2023-11-27 00:43:52 +0100

    corrected opti for mobile version

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -4,7 +4,7 @@ const ProfilePicture = ({ imageUrl }) => {
   return (
     <div className="flex justify-center items-center">
       <div
-        className={`w-24 h-24 md:w-32 md:h-32 bg-gradient-to-r from-blue-400 to-purple-600 rounded-full overflow-hidden p-1 shadow-lg ${"hide-on-height"}`}
+        className={`w-24 h-24 md:w-32 md:h-32 bg-gradient-to-r from-blue-400 to-purple-600 rounded-full overflow-hidden p-1 shadow-lg`}
       >
         <img
           src={imageUrl}
